4bdc2c67ea9cd416cbf6ccf87feef542defde86d
Chris Bell <cb.camellia@gmail.com>
2020-08-08 13:13:50 -0400

    Add hash function for opcode lookup

--- interpreter.py
+++ interpreter.py
@@ -20,33 +20,47 @@ class Interpreter:
         self.stack = np.array([0x0] * 0xF, np.uint16)
         # Reference to external display
         self.display = display
-        # Use a dictionary to quickly look up instructions
-        self.instruction = [0] * 0xF5
-        self.instruction[80] = self._8XY0
+        # Use a dictionary to look up which function to use for any opcode
+        # https://en.wikipedia.org/wiki/CHIP-8
+        self.opcode = {}
+        self.opcode[self.hash_opcode(0x10, 0x9E)] = self._8XY0
+        self.opcode[self.hash_opcode(0x00, 0x00)] = 1
+        self.opcode[self.hash_opcode(0x00, 0x00)] = 1
 
 
     def load_program_to_memory(self, file):
         """Stores the program in the memory buffer"""
         bin = bytes(file.read())
         program = bin.hex()
-        # Each instruction must be 2 bytes long (4 characters)
+        # Each opcode must be 2 bytes long (4 characters)
         if not len(program) % 4 == 0:
             messagebox.showerror("Invalid ROM", "That wasn't a Chip-8 ROM...")
             return
 
         print(len(program))
-        # Store each individual instruction in 2 bytes of RAM starting at 0x200
+        # Store each individual opcode in 2 bytes of RAM starting at 0x200
         for i in range(int(len(program) / 4)):
-            instruction = program[i*4:i*4+4]
-            # Need to split instruction in half to fit in a byte
-            self.memory_buffer[0x200 + i*2] = (int(instruction[:2], 16))
-            self.memory_buffer[0x200 + (i*2)+1] = (int(instruction[2:], 16))
+            opcode = program[i*4:i*4+4]
+            # Need to split opcode in half to fit in a byte
+            self.memory_buffer[0x200 + i*2] = (int(opcode[:2], 16))
+            self.memory_buffer[0x200 + (i*2)+1] = (int(opcode[2:], 16))
 
     def set_register(self, register, value):
         """Set a register to a value"""
 
     def execute_instruction(self):
         """Executes the current instruction in the program counter register"""
+
+    def hash_opcode(self, upper_byte, lower_byte):
+        """Get an ID for a 2-byte opcode using the first and last 4 bits"""
+        upper_byte = upper_byte >> 4 & 0b1111
+        lower_byte = lower_byte & 0b1111
+        print(bin(upper_byte)[2:].zfill(8))
+        print(bin(lower_byte)[2:].zfill(8))
+        # Concatenate the bits
+        id = upper_byte << 4 | lower_byte
+        print(id)
+        return 0
 
     def _8XY0(self, x, y):
         """Sets value of VX to VY"""
