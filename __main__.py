72f82a76dba5b9c4a7491b4527fd6bdd6d2635b8
Chris Bell <chrisbell.dev@gmail.com>
2020-11-09 20:24:34 -0800

    Add licensing and copyright notice

--- __main__.py
+++ __main__.py
@@ -1,3 +1,27 @@
+"""
+MIT License
+
+Copyright (c) 2020 Chris Bell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import tkinter
 from window import Window
 from interpreter import Interpreter
@@ -5,6 +29,15 @@ from renderer import Renderer
 from file_io import FileIO
 from keymap import Keymap
 from audio import AudioSynthesizer
+
+copyright_frame = None
+
+def show_copyright(root, width, height):
+    copyright_frame = tkinter.Toplevel(root, width=width, height=height)
+    copyright_frame.title("Copyright Info")
+    label = tkinter.Label(copyright_frame, text="Copyright 2020, Chris Bell", fg="red")
+    label.pack();
+    copyright_frame.protocol("WM_DELETE_WINDOW", copyright_frame.destroy)
 
 def main():
 
@@ -41,6 +74,7 @@ def main():
     file_menu.add_command(label="Save state...", command=file_io.save_state)
     file_menu.add_command(label="Load state...", command=file_io.load_state)
     file_menu.add_command(label="Keyboard settings...", command=keymap.open_window)
+    file_menu.add_command(label="About...", command=lambda: show_copyright(root, 720, 480))
 
     file_menu.add_separator()
 
